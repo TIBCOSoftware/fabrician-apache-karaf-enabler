5f33bc7c88985505e074408f6faf26d89678c9ac
blau <blau@BKLAU-W510.na.tibco.com>
2013-07-25 14:48:36 -0400

    Commits preamble

---
+++ src/main/java/org/fabrician/enabler/KarafLoggingInfo.java
@@ -0,0 +1,19 @@
+/*
+* Copyright (c) 2013 TIBCO Software Inc. All Rights Reserved.
+*
+* Use is subject to the terms of the TIBCO license terms accompanying the download of this code.
+* In most instances, the license terms are contained in a file named license.txt.
+*/
+package org.fabrician.enabler;
+
+import com.datasynapse.fabric.domain.featureinfo.ApplicationLoggingInfo;
+
+public class KarafLoggingInfo extends ApplicationLoggingInfo {
+    private static final long serialVersionUID = -2029102510943271808L;
+    public static final String[] DEFAULT_PATTERNS = { "${KARAF_HOME}/data/log/.*\\.log" };
+
+    protected String[] getDefaultPatterns() {
+        return DEFAULT_PATTERNS;
+    }
+
+}
